f1df9587fd9945dd009a57eb87b643aab4be78ce
MattBusscher <mattybusscher@gmail.com>
2023-07-25 06:06:26 +0000

    added npm and tailwind

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # mjb-flask-portfolio
 
-## Personal Flask site for my Profesional Portfolio
+Personal Flask site for my Profesional Portfolio
 
+### tailwindcss
+```npm run build```
 By Matt Busscher
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'red': '#703030',
+        'grey': '#2F343B',
+        'green': '#7E827A',
+        'teak': '#E3CDA4',
+        'maroon': '#C77966',
+      }
+    },
+  },
+  plugins: [],
+}
+
